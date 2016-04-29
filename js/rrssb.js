65f5524d644da15a81db7cdbe1c9b6d626f5a7be
AdamPS <id@ashepherd.co.uk>
2016-04-30 00:56:33 +0100

    Fix whitespace

--- js/rrssb.js
+++ js/rrssb.js
@@ -18,117 +18,117 @@
 */
 
 +(function(window, $, undefined) {
-	'use strict';
+    'use strict';
 
-	/*
-	 * Public Function
-	 */
+    /*
+     * Public Function
+     */
 
-	 $.fn.rrssb = function( options ) {
+    $.fn.rrssb = function( options ) {
 
-		// Settings that $.rrssb() will accept.
-		var settings = $.extend({
-			description: undefined,
-			emailAddress: undefined,
-			emailBody: undefined,
-			emailSubject: undefined,
-			image: undefined,
-			title: undefined,
-			url: undefined
-		}, options );
+      // Settings that $.rrssb() will accept.
+      var settings = $.extend({
+          description: undefined,
+          emailAddress: undefined,
+          emailBody: undefined,
+          emailSubject: undefined,
+          image: undefined,
+          title: undefined,
+          url: undefined
+      }, options );
 
-	};
+    };
 
-	function rrssbFix() {
-	  var origWidth = 0;
-	  var buttons = 0;
-	  $('li', this).each(function() {
-	      origWidth += +$(this).attr('orig-width');
-	      buttons++;
-	  });
-	  var containerWidth = $(this).width();
-	  var fontSize = origWidth / buttons / 12;
-	  if (containerWidth < origWidth) {
-			$(this).addClass('small-format');
-			$('li', this).addClass('small');
-		}
-		else {
-			$(this).removeClass('small-format');
-			$('li', this).removeClass('small');
-			// enlarge font size if buttons get wide enough
-			//$(this).css('font-size', fontSize + 'px');
-			//$(this).css('font-size', '');
-		}
-	}
-	
-	var rrssbInit = function() {
-		$('.rrssb-buttons').each(function(index) {
-			$(this).addClass('rrssb-'+(index + 1));
-		});
+    function rrssbFix() {
+      var origWidth = 0;
+      var buttons = 0;
+      $('li', this).each(function() {
+          origWidth += +$(this).attr('orig-width');
+          buttons++;
+      });
+      var containerWidth = $(this).width();
+      var fontSize = origWidth / buttons / 12;
+      if (containerWidth < origWidth) {
+        $(this).addClass('small-format');
+        $('li', this).addClass('small');
+      }
+      else {
+        $(this).removeClass('small-format');
+        $('li', this).removeClass('small');
+        // enlarge font size if buttons get wide enough
+        //$(this).css('font-size', fontSize + 'px');
+        //$(this).css('font-size', '');
+      }
+    }
 
-		//setPercentBtns();
-		
-		// grab initial width of each button and add as data attr
-		$('.rrssb-buttons li').each(function() {
-			$(this).attr('orig-width', $(this).innerWidth());
-		});
-		
-		$('.rrssb-buttons').each(rrssbFix);
-	};
+    var rrssbInit = function() {
+      $('.rrssb-buttons').each(function(index) {
+          $(this).addClass('rrssb-'+(index + 1));
+      });
 
-	var popupCenter = function(url, title, w, h) {
-		// Fixes dual-screen position                         Most browsers      Firefox
-		var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-		var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+      //setPercentBtns();
 
-		var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-		var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+      // grab initial width of each button and add as data attr
+      $('.rrssb-buttons li').each(function() {
+          $(this).attr('orig-width', $(this).innerWidth());
+      });
 
-		var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-		var top = ((height / 3) - (h / 3)) + dualScreenTop;
+      $('.rrssb-buttons').each(rrssbFix);
+    };
 
-		var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    var popupCenter = function(url, title, w, h) {
+      // Fixes dual-screen position                         Most browsers      Firefox
+      var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+      var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
 
-		// Puts focus on the newWindow
-		if (window.focus) {
-			newWindow.focus();
-		}
-	};
+      var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+      var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
-	var waitForFinalEvent = (function () {
-		var timers = {};
-		return function (callback, ms, uniqueId) {
-			if (!uniqueId) {
-				uniqueId = "Don't call this twice without a uniqueId";
-			}
-			if (timers[uniqueId]) {
-				clearTimeout (timers[uniqueId]);
-			}
-			timers[uniqueId] = setTimeout(callback, ms);
-		};
-	})();
+      var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+      var top = ((height / 3) - (h / 3)) + dualScreenTop;
 
-	// init load
-	$(document).ready(function(){
-		/*
-		 * Event listners
-		 */
+      var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 
-		$(document).click('.rrssb-buttons a.popup', {}, function popUp(e) {
-			var self = $(this);
-			popupCenter(self.attr('href'), self.find('.rrssb-text').html(), 580, 470);
-			e.preventDefault();
-		});
+      // Puts focus on the newWindow
+      if (window.focus) {
+        newWindow.focus();
+      }
+    };
 
-		// resize function
-		$(window).resize(function () {
-			waitForFinalEvent(function() {$('.rrssb-buttons').each(rrssbFix);}, 200, "finished resizing");
-		});
+    var waitForFinalEvent = (function () {
+        var timers = {};
+        return function (callback, ms, uniqueId) {
+          if (!uniqueId) {
+            uniqueId = "Don't call this twice without a uniqueId";
+          }
+          if (timers[uniqueId]) {
+            clearTimeout (timers[uniqueId]);
+          }
+          timers[uniqueId] = setTimeout(callback, ms);
+        };
+    })();
 
-		rrssbInit();
-	});
+    // init load
+    $(document).ready(function(){
+        /*
+        * Event listners
+        */
 
-	// Make global
-	window.rrssbInit = rrssbInit;
+        $(document).click('.rrssb-buttons a.popup', {}, function popUp(e) {
+            var self = $(this);
+            popupCenter(self.attr('href'), self.find('.rrssb-text').html(), 580, 470);
+            e.preventDefault();
+        });
+
+        // resize function
+        $(window).resize(function () {
+            waitForFinalEvent(function() {$('.rrssb-buttons').each(rrssbFix);}, 200, "finished resizing");
+        });
+
+        rrssbInit();
+    });
+
+    // Make global
+    window.rrssbInit = rrssbInit;
 
 })(window, jQuery);
