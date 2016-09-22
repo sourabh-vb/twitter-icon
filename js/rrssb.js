4735edcb4e740167967eaf711b04ccc89538e16a
AdamPS <id@ashepherd.co.uk>
2016-09-22 11:39:06 +0100

    Script largely working for multi-row responsiveness

--- js/rrssb.js
+++ js/rrssb.js
@@ -1,80 +1,89 @@
 /*!
- Ridiculously Responsive Social Sharing Buttons
- Team: @dbox, @joshuatuscan
- Site: http://www.kurtnoble.com/labs/rrssb
- Twitter: @therealkni
-
-        ___           ___
-       /__/|         /__/\        ___
-      |  |:|         \  \:\      /  /\
-      |  |:|          \  \:\    /  /:/
-    __|  |:|      _____\__\:\  /__/::\
-   /__/\_|:|____ /__/::::::::\ \__\/\:\__
-   \  \:\/:::::/ \  \:\~~\~~\/    \  \:\/\
-    \  \::/~~~~   \  \:\  ~~~      \__\::/
-     \  \:\        \  \:\          /__/:/
-      \  \:\        \  \:\         \__\/
-       \__\/         \__\/
+ Simple Responsive Social Sharing Buttons
 */
 
 +(function(window, $, undefined) {
     'use strict';
+    // @@ Use of global
+    var settings = {
+      shrink: 0.7,
+      minRows: 1,
+      maxRows: 2,
+    };
 
     /*
      * Public Function
      */
+    $.fn.rrssb = function(options) {
 
-    $.fn.rrssb = function( options ) {
-
-      // Settings that $.rrssb() will accept.
-      var settings = $.extend({
-          description: undefined,
-          emailAddress: undefined,
-          emailBody: undefined,
-          emailSubject: undefined,
-          image: undefined,
-          title: undefined,
-          url: undefined
-      }, options );
+      // Settings that $.rrssb() will accept.  @@ Test
+      settings = $.extend(settings, options);
 
     };
 
-    function rrssbFix() {
-      var origWidth = 0;
-      var buttons = 0;
-      $('li', this).each(function() {
-          origWidth += +$(this).attr('orig-width');
-          buttons++;
-      });
-      var containerWidth = $(this).width();
-      var fontSize = origWidth / buttons / 12;
-      if (containerWidth < origWidth) {
-        $(this).addClass('small-format');
-        $('li', this).addClass('small');
-      }
-      else {
-        $(this).removeClass('small-format');
-        $('li', this).removeClass('small');
-        // enlarge font size if buttons get wide enough
-        //$(this).css('font-size', fontSize + 'px');
-        //$(this).css('font-size', '');
-      }
-    }
-
-    var rrssbInit = function() {
+    function rrssbInit() {
       $('.rrssb-buttons').each(function(index) {
           $(this).addClass('rrssb-'+(index + 1));
-      });
-
-      //setPercentBtns();
-
-      // grab initial width of each button and add as data attr
-      $('.rrssb-buttons li').each(function() {
-          $(this).attr('orig-width', $(this).innerWidth());
+          var buttonWidth = 0;
+          $('li', this).each(function() {
+              buttonWidth = Math.max(buttonWidth, $(this).innerWidth());
+          });
+          // Set all buttons to match width of largest.
+          // This width stays no matter what sizing, but it may get constrained down by a max-width.
+          // In the case where the buttons are in a float with no fixed width, having the full
+          // width set on each button ensures that the float is able to grow back up from no-labels to having labels again.
+          $('li', this).width(buttonWidth);
+          $(this).attr('orig-width', buttonWidth);
+          $(this).attr('orig-height', $('li', this).innerHeight());
+          $(this).attr('orig-font-size', parseFloat($(this).css("font-size")));
       });
 
       $('.rrssb-buttons').each(rrssbFix);
     };
+
+    function rrssbFix() {
+      var buttonWidth = $(this).attr('orig-width');
+      var buttons = $('li', this).length;
+      // Modern browsers have sub-pixel support, so an element can have a fractional width internally.
+      // This can get rounded up in the result of innerWidth, so subtract 1px to get a safe width.
+      var containerWidth = $(this).innerWidth() - 1;
+      var buttonsPerRow = Math.floor(containerWidth / (buttonWidth * settings.shrink));
+      var rowsNeeded = Math.ceil(buttons / buttonsPerRow);
+
+      // Fix labels.
+      if (rowsNeeded > settings.maxRows) {
+        $(this).addClass('no-label');
+        buttonWidth = $(this).attr('orig-height');
+        buttonsPerRow = Math.floor(containerWidth / (buttonWidth * settings.shrink));
+        rowsNeeded = Math.ceil(buttons / buttonsPerRow);
+      }
+      else {
+        $(this).removeClass('no-label');
+      }
+
+      // Fix sizes.
+      // Can't use a percent width as it doesn't work well if the buttons are inside a non-fixed-size container.
+      buttonsPerRow = Math.ceil(buttons / rowsNeeded);
+      var calcWidth = Math.floor(Math.min(buttonWidth, containerWidth / buttonsPerRow));
+      var percWidth = Math.floor(10000 / buttonsPerRow) / 100;
+      $('li', this).css('max-width', percWidth + '%');
+
+      if (calcWidth < buttonWidth) {
+        // Reduce font size.  Take account of padding, which is a fixed amount.
+        // Reduce calculated value slightly as browser size calculations have some rounding and approximation.
+        var buttonPadding = $('li', this).innerWidth() - $('li', this).width();
+        var scale = (calcWidth - buttonPadding) / (buttonWidth - buttonPadding);
+        var fontSize = $(this).attr('orig-font-size') * scale  * 0.97;
+        $(this).css('font-size', fontSize + 'px');
+        $(this).css('padding-right', '');
+      }
+      else {
+        // Set padding to ensure the buttons wrap evenly, for example 6 => 3+3 not 4+2.
+        var padding = containerWidth - buttonsPerRow * buttonWidth;
+        $(this).css('padding-right', padding + 'px');
+        $(this).css('font-size', '');
+      }
+    }
 
     var popupCenter = function(url, title, w, h) {
       // Fixes dual-screen position                         Most browsers      Firefox
@@ -127,8 +136,5 @@
 
         rrssbInit();
     });
-
-    // Make global
-    window.rrssbInit = rrssbInit;
 
 })(window, jQuery);
