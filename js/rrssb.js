61d53dfee1e441d266d602e6467b6ce6c00d6a98
AdamPS <id@ashepherd.co.uk>
2016-10-09 08:14:41 +0100

    - Add settings.size - Tidy up settings initialization - Auto-initialize buttons

--- js/rrssb.js
+++ js/rrssb.js
@@ -4,12 +4,13 @@
 
 +(function(window, $, undefined) {
     'use strict';
-    var defaults = {
-      shrink: 0.8,
-      regrow: 0.8,
-      minRows: 1,
-      maxRows: 2,
-      maxPrefix: 0.3,
+    var schema = {
+      size: {min: 0.1, max: 10, default: 1},
+      shrink: {min: 0.2, max: 1, default: 0.8},
+      regrow: {min: 0.2, max: 1, default: 0.8},
+      minRows: {min: 1, max: 99, default: 1},
+      maxRows: {min: 1, max: 99, default: 2},
+      maxPrefix: {min: 0, max: 0.8, default: 0.3},
     };
 
     /**
@@ -26,14 +27,20 @@
      * $(this) points to an instance of .rrssb
      */
     $.fn.rrssbConfig = function(settings) {
-      var checkedSettings = $.extend({}, defaults);
-      if (settings) {
-        if (settings.shrink >= 0.2 && settings.shrink <= 1) checkedSettings.shrink = settings.shrink;
-        if (settings.regrow >= 0.2 && settings.regrow <= 1) checkedSettings.regrow = settings.regrow;
-        if (settings.minRows >= 1 && settings.minRows <= 99) checkedSettings.minRows = settings.minRows;
-        if (settings.maxRows >= 1 && settings.maxRows <= 99) checkedSettings.maxRows = settings.maxRows;
-        if (settings.maxPrefix >= 0 && settings.shrink <= 0.8) checkedSettings.maxPrefix = settings.maxPrefix;
+      if ($(this).data('settings') && !settings) {
+        return;
       }
+
+      var checkedSettings = {};
+      for (var param in schema) {
+        if (settings && settings[param] !== '') {
+         checkedSettings[param] = Math.min(schema[param].max, Math.max(schema[param].min, settings[param]));
+        }
+        else {
+          checkedSettings[param] = schema[param].default;
+        }
+      }
+
       $(this).data('settings', checkedSettings);
       rrssbFix.call(this);
     };
@@ -43,11 +50,6 @@
      * $(this) points to an instance of .rrssb
      */
     var rrssbInit = function() {
-      if (!$(this).data('settings')) {
-        // Initialise default settings.
-        $(this).data('settings', defaults);
-      }
-
       // Store original values.
       var orig = {
         width: 0,
@@ -62,12 +64,6 @@
         orig.buttons++;
       });
 
-      // Set all buttons to match width of largest.
-      // This width stays no matter what sizing, but it may get constrained down by a max-width.
-      // In the case where the buttons are in a float with no fixed width, having the full
-      // width set on each button ensures that the float is able to grow back up from no-labels to having labels again.
-      $('li', this).width(orig.width);
-
       $(this).data('orig', orig);
       return orig;
     }
@@ -75,36 +71,35 @@
     /**
      * Fix all sets of buttons on the page.
      */
-    var fixAll = function(initial) {
-      $('.rrssb').each(function(){
-          rrssbFix.call(this);
-      });
+    var fixAll = function() {
+      $('.rrssb').each(rrssbFix);
     }
 
     /**
      * Main recalculte sizes function.
      * $(this) points to an instance of .rrssb
      */
-    var rrssbFix = function(initial) {
+    var rrssbFix = function() {
       var orig = $(this).data('orig');
       if (!orig) {
         orig = rrssbInit.call(this);
       }
-      else if (initial) {
-        return;
-      }
       var settings = $(this).data('settings');
-      var buttonWidth = orig.width;
+      var buttonWidth = orig.width * settings.size;
       var buttons = orig.buttons;
 
       // Modern browsers have sub-pixel support, so an element can have a fractional width internally.
       // This can get rounded up in the result of innerWidth, so subtract 1px to get a safe width.
       var containerWidth = $(this).innerWidth() - 1;
 
-      // The container can't shrink below the size of one button.
+      // Set all buttons to match width of largest.
+      // This width stays no matter what sizing, but it may get constrained down by a max-width.
+      // In the case where the buttons are in a float with no fixed width, having the full
+      // width set on each button ensures that the float is able to grow back up from no-labels to having labels again.
+      // However, the container can't shrink below the size of one button.
       // For small containers make sure we have small buttons.
       // After changing this we need to let the browser recalculate then run again.
-      var mini = (containerWidth <= orig.width);
+      var mini = (containerWidth <= buttonWidth);
       var lastMini = $(this).data('mini');
       if (mini != lastMini) {
         $(this).data('mini', mini);
@@ -114,28 +109,28 @@
           $(this).addClass('no-label');
         }
         else {
-          $('li', this).width(orig.width);
+          $('li', this).width(buttonWidth);
         }
 
-        timer = setTimeout(fixAll, 1);
+        delayedFixAll(1);
         return;
       }
 
-      var prefixWidth = orig.prefixWidth;
+      var prefixWidth = orig.prefixWidth * settings.size;
       if (prefixWidth > containerWidth * settings.maxPrefix) {
         prefixWidth = 0;
       }
 
-      var maxButtonWidth = containerWidth / settings.shrink - prefixWidth;
-      var buttonsPerRow = Math.floor(maxButtonWidth / buttonWidth);
+      var availWidth = containerWidth / settings.shrink - prefixWidth;
+      var buttonsPerRow = Math.floor(availWidth / buttonWidth);
       var rowsNeeded = Math.max(settings.minRows, Math.ceil(buttons / buttonsPerRow));
 
       // Fix labels.
       if (rowsNeeded > settings.maxRows) {
         $(this).addClass('no-label');
         // Without label, button is square so width equals original height.
-        buttonWidth = orig.height;
-        buttonsPerRow = Math.max(1, Math.floor(maxButtonWidth / buttonWidth));
+        buttonWidth = orig.height * settings.size;
+        buttonsPerRow = Math.max(1, Math.floor(availWidth / buttonWidth));
         rowsNeeded = Math.max(settings.minRows, Math.ceil(buttons / buttonsPerRow));
       }
       else {
@@ -154,15 +149,13 @@
         scale = Math.min(scale, settings.regrow);
       }
 
+      var fontSize = orig.fontSize * settings.size;
       if (scale < 1) {
         // Reduce font size.
         // Reduce calculated value slightly as browser size calculations have some rounding and approximation.
-        var fontSize = orig.fontSize * scale * 0.95;
-        $(this).css('font-size', fontSize + 'px');
+        fontSize *= scale * 0.95;
       }
-      else {
-        $(this).css('font-size', '');
-      }
+      $(this).css('font-size', fontSize + 'px');
 
       desiredWidth *= scale;
       if (containerWidth > desiredWidth) {
@@ -174,6 +167,8 @@
       else {
         $(this).css('padding-right', '');
       }
+
+      //$(this).css('padding-left', prefixWidth * scale + 'px');
     };
 
     var popupCenter = function(url, title, w, h) {
@@ -196,17 +191,17 @@
     };
 
     var timer;
-    var delayedFixAll = function () {
+    var delayedFixAll = function(ms) {
       if (timer) {
         clearTimeout(timer);
       }
-      timer = setTimeout(fixAll, 200);
+      timer = setTimeout(fixAll, ms ? ms : 200);
     };
 
     /**
      * Ready function
      */
-    $(document).ready(function(){
+    $(document).ready(function() {
         // Register event listners
         $('.rrssb-buttons a.popup').click(function popUp(e) {
             popupCenter($(this).attr('href'), $(this).find('.rrssb-text').html(), 580, 470);
@@ -214,6 +209,10 @@
         });
 
         $(window).resize(delayedFixAll);
+
+        // Add another ready callback that will be called after all others.
+        // Configure any buttons that haven't already been configured.
+        $(document).ready(configAll);
     });
 
 })(window, jQuery);
